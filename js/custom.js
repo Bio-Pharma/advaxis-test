211fc5da9e20edc1ff4635176b9e28da93ec95b1
Bio-Pharma <desair@bio-pharma.com>
2018-01-11 15:47:39 +0600

    Topbar form conditions remove from js file

--- js/custom.js
+++ js/custom.js
@@ -11,3 +11,22 @@
 	    	$('#wrapper').removeClass('active');
 	    }
     });
+
+   /*
+    $('#top_input').keyup(function(){
+        var val = $(this).val();
+        if(val != ''){
+            $(this).parent('.top-form').find('#top_link').removeAttr("disabled");
+            $(this).parent('.top-form').find('#top_link').css("pointer-events","auto");
+            //$("#login_btn").removeAttr("disabled");
+            //$("#login_btn").css("pointer-events","auto");
+        }
+        else if(val == ''){
+            $(this).parent('.top-form').find('#top_link').attr("disabled");
+            $(this).parent('.top-form').find('#top_link').css("pointer-events","none");
+            //$("#login_btn").attr("disabled");
+            //$("#login_btn").css("pointer-events","none");
+
+        }
+    });
+    */
